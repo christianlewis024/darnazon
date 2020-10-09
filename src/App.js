6a264ef2a19033fa4f01c91c726029f7abc89730
Christian Lewis <christianlewis024@gmail.com>
2020-10-09 14:02:47 -0700

    added banner image and a mask fading gradiant to the image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
- import Header from "./Header"
+import Header from "./Header"
+import Home from "./Home"
 function App() {
   return (
     <Router>
@@ -20,7 +21,7 @@ function App() {
       {/* "/" will be the default home page route */}
       <Route path="/">
         <Header/>
-        <h1>Home Page</h1>
+        <Home/>
       </Route>
      </Switch>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Home.css"
+function Home() {
+    return (
+        <div className="home">           
+            <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020_PrimeDay/GW/RB-2817_PD2020_Channels_GW_Desktop_Tablet_Hero_Biz_EN_V2b_1x._CB403910725_.jpg" alt="amazon prime banner add" />
+        </div>
+    )
+}
+
+export default Home
