e39425226d7c9f67bf1691ad05037aa7fd322b7c
Christian Lewis <christianlewis024@gmail.com>
2020-10-09 11:30:17 -0700

    added styling to the header and image is a link

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Header.css"
 
 function Header() {
     return (
         <nav className="header">
             {/* logo on the left -> img */}
+            <Link to="/">
             <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
             {/*search box  */}
             {/* 3Links: sign in, returns/orders, yourPrime */}
             {/*basket icon/shopping cart */}
-
+            </Link>
 
         </nav>
     )
