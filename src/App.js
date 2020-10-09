13141d5c33df7f1804494d7aaa34ac8d915f781c
Christian Lewis <christianlewis024@gmail.com>
2020-10-09 13:16:53 -0700

    added nav bar to all pages

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ function App() {
      <Switch>       
        {/* "/checkout" route for the checkout page */}
       <Route path="/checkout">
+      <Header/>
         <h1>checkout</h1>
       </Route>
       {/* "/login" route for the login/auth page */}
       <Route path="/login">
+      <Header/>
         <h1>login</h1>
       </Route>
       {/* "/" will be the default home page route */}
