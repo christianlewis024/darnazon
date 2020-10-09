4f992f8d45f13885d054071c08459c6495e82a98
Christian Lewis <christianlewis024@gmail.com>
2020-10-09 11:02:06 -0700

    added a header component and header image

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* logo on the left -> img */}
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+            {/*search box  */}
+            {/* 3Links: sign in, returns/orders, yourPrime */}
+            {/*basket icon/shopping cart */}
+
+
+        </nav>
+    )
+}
+
+export default Header
