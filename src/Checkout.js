3b4f53c91952ddd308c2f886da8ea502eb6e7d71
Christian Lewis <christianlewis024@gmail.com>
2020-10-09 21:30:00 -0700

    added subtotal and checkout button + styling

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { useStateValue} from "./StateProvider"
 import "./Checkout.css"
 import CheckoutProduct from "./CheckoutProduct"
+import Subtotal from "./Subtotal"
+
 
 function Checkout({ item, title, image, price, rating}) {
     const [{basket}] = useStateValue();
     return (
         <div className="checkout">
+            <div className="checkout__left">
            <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/01/PAE/Benito/Banners/041320_Acquisition_Banner_GCLP_map_learn._CB1198675309_.jpg"/>
            
 
@@ -31,6 +34,12 @@ function Checkout({ item, title, image, price, rating}) {
                  ))}
              </div> 
           ) } 
+          </div>
+          {basket.length > 0 &&  (
+              <div className="checkout__right">
+                <Subtotal/>
+              </div>
+          )}
         </div>
     )
 }
